9d0e3b9bc6d6434462009382330870e4dae9c281
Mahnoor Ismail <mahnoorhamidismail@gmail.com>
2024-03-02 20:29:13 +0500

    added coloring functionality and refactored code

--- index.js
+++ index.js
@@ -1,26 +1,63 @@
-// Variables:
+// VARIABLES
 const setGridBtn = document.getElementById("create-grid-btn");
 const resetGridBtn = document.getElementById("reset-grid-btn");
 const gridSizeInp = document.getElementById("grid-input");
 const gridContainer = document.getElementById("grid");
-const gridBoxes = document.querySelectorAll(".grid-box");
 
-// Event Listeners:
-document.addEventListener("DOMContentLoaded", function () {
-  for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-      // Create a grid Box element
+// FUNCTIONS
+
+// Creating the grid:
+
+function colorGridBox() {
+  let gridBoxes = document.querySelectorAll(".grid-box");
+  gridBoxes.forEach(function (gridBox) {
+    gridBox.addEventListener("mouseenter", function () {
+      gridBox.style.backgroundColor = generateRandomRGB();
+    });
+  });
+}
+
+function createGrid(size) {
+  gridContainer.innerHTML = "";
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      // Creating a grid Box element
       const gridBox = document.createElement("div");
       gridBox.className = "grid-box";
-      gridBox.style.width = `${100 / 16}%`;
-      gridBox.style.height = `${100 / 16}%`;
-
-      // Append the grid item to the grid container
+      gridBox.style.width = `${100 / size}%`;
+      gridBox.style.height = `${100 / size}%`;
+      // Appending the grid item to the grid container
       gridContainer.appendChild(gridBox);
     }
   }
+}
+
+// Default settings:
+function toDefault() {
+  gridSizeInp.value = "";
+  createGrid(16);
+  colorGridBox();
+}
+
+// Generating random rbg value:
+function generateRandomRGB() {
+  let r, g, b;
+  do {
+    r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+  } while (r + g + b >= 700); // to avoid white
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+// EVENT LISTENERS
+
+// When DOM loads:
+document.addEventListener("DOMContentLoaded", function () {
+  toDefault();
   gridSizeInp.addEventListener("input", function () {
-    // Check if the input field is empty
+    // Checking if the input field is empty
     if (
       gridSizeInp.value.trim() === "" ||
       parseInt(gridSizeInp.value.trim()) < 1 ||
@@ -39,18 +76,20 @@ document.addEventListener("DOMContentLoaded", function () {
     setGridBtn.classList.add("disabled");
     setGridBtn.disabled = true;
   }
-
-  gridSizeInp.focus();
 });
 
-// Allow button click with Enter key:
+// 'Create Grid' button click with 'Enter' key:
 gridSizeInp.addEventListener("keydown", function (event) {
   if (event.key === "Enter" && !setGridBtn.disabled) {
     setGridBtn.click();
   }
 });
 
+// 'Create Grid' button click functionality:
 setGridBtn.addEventListener("click", function () {
+  setGridBtn.blur();
+
+  // Validating user input:
   if (
     parseInt(gridSizeInp.value.trim()) < 1 ||
     parseInt(gridSizeInp.value.trim()) > 100
@@ -58,18 +97,15 @@ setGridBtn.addEventListener("click", function () {
     alert("Please enter a value between 1 and 100");
     return;
   }
-  // Clear previously created grid boxes
-  gridContainer.innerHTML = "";
-  for (let i = 0; i < gridSizeInp.value; i++) {
-    for (let j = 0; j < gridSizeInp.value; j++) {
-      // Create a grid Box element
-      const gridBox = document.createElement("div");
-      gridBox.className = "grid-box";
-      gridBox.style.width = `${100 / gridSizeInp.value}%`;
-      gridBox.style.height = `${100 / gridSizeInp.value}%`;
 
-      // Append the grid item to the grid container
-      gridContainer.appendChild(gridBox);
-    }
-  }
+  // Clearing previously created grid boxes:
+  gridContainer.innerHTML = "";
+  createGrid(gridSizeInp.value);
+  colorGridBox();
+});
+
+// Reset button functionality (sets the page back to default settings):
+resetGridBtn.addEventListener("click", function () {
+  resetGridBtn.blur();
+  toDefault();
 });
